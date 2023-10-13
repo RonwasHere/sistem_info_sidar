bf872ed8dbb73bc486eb842409e300da7431055b
ronald <ronaldliansugihartono@gmail.com>
2023-10-13 16:38:44 +0700

    feat = edit suffixicon

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,99 +11,92 @@ class _LoginPageState extends State<LoginPage> {
   bool obscureText = true;
   @override
   void initState() {
-    // _passwordVisible = false;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Expanded(
-        child: Scaffold(
-          backgroundColor: Color(0xff101317),
-          body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(
-                  'assets/icon_indraco.png',
-                  width: 80,
-                  height: 80,
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Text(
-                      'Welcome Back',
-                      style: TextStyle(fontSize: 22, color: Colors.white),
+      child: Scaffold(
+        backgroundColor: Color(0xff101317),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/icon_indraco.png',
+                width: 80,
+                height: 80,
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Text(
+                    'Welcome Back',
+                    style: TextStyle(fontSize: 22, color: Colors.white),
+                  ),
+                  SizedBox(width: 10),
+                  Icon(
+                    Icons.waving_hand_outlined,
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                'to HR Attendee',
+                style: TextStyle(fontSize: 22, color: Color(0xff3085fe)),
+              ),
+              Text(
+                'Hello There, login to continue',
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                style: TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                  hintText: 'Email@gmail.com',
+                  hintStyle: TextStyle(color: Colors.white),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Color(0xff3085fe),
                     ),
-                    SizedBox(width: 10),
-                    Icon(
-                      Icons.waving_hand_outlined,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Color(0xff3085fe),
                     ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'to HR Attendee',
-                  style: TextStyle(fontSize: 22, color: Color(0xff3085fe)),
-                ),
-                Text(
-                  'Hello There, login to continue',
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ),
-                SizedBox(height: 15),
-                TextFormField(
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                  decoration: InputDecoration(
-                    hintText: 'Email@gmail.com',
-                    hintStyle: TextStyle(color: Colors.white),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                        color: Color(0xff3085fe),
-                      ),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                        color: Color(0xff3085fe),
-                      ),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
-                SizedBox(height: 15),
-                TextFormField(
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                  decoration: InputDecoration(
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        obscureText = !obscureText;
-                      },
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                style: TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.visibility),
+                  suffixIconColor: Color(0xff3085fe),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Color(0xff3085fe),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                        color: Color(0xff3085fe),
-                      ),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                        color: Color(0xff3085fe),
-                      ),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  obscureText: obscureText,
-                  // _passwordVisible = false;
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Color(0xff3085fe),
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
-              ],
-            ),
+                obscureText: obscureText,
+              ),
+            ],
           ),
         ),
       ),
