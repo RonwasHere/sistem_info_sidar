cc2d04df2a00fbe807b89c6107e7a326c86e1695
ronald <ronaldliansugihartono@gmail.com>
2023-12-01 16:43:04 +0700

    feat = login page done...push to github

---
+++ lib/dashboard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              "1",
+              style: TextStyle(fontSize: 70),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -29,52 +29,61 @@ class _LoginPageState extends State<LoginPage> {
                 width: 80,
                 height: 80,
               ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Text(
-                    'Welcome Back',
-                    style: TextStyle(fontSize: 22, color: Colors.white),
+              SizedBox(height: 15),
+              Center(
+                child: Text(
+                  'S I D A R',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                  SizedBox(width: 10),
-                  Icon(
-                    Icons.waving_hand_outlined,
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Text(
-                'to HR Attendee',
-                style: TextStyle(fontSize: 22, color: Color(0xff3085fe)),
-              ),
-              Text(
-                'Hello There, login to continue',
-                style: TextStyle(fontSize: 15, color: Colors.white),
+                ),
               ),
               SizedBox(height: 15),
+              Center(
+                child: Text(
+                  'SYSTEM INFORMATION',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Center(
+                child: Text(
+                  'DAILY ACTIVITY REPORT',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 25),
+              //TEXTFORMFIELD EMAIL
               TextFormField(
+                maxLength: 30,
                 style: TextStyle(color: Colors.white, fontSize: 20),
                 decoration: InputDecoration(
                   hintText: 'Email@gmail.com',
                   hintStyle: TextStyle(color: Colors.white),
                   enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 3,
-                      color: Color(0xff3085fe),
-                    ),
-                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(width: 3, color: Colors.white),
+                    
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
                       width: 3,
-                      color: Color(0xff3085fe),
+                      color: Colors.white,
                     ),
-                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
               ),
-              SizedBox(height: 15),
+              SizedBox(height: 25),
+              //TEXTFORMFIELD PASSWORD
               TextFormField(
+                maxLength: 6, //pembatasan hanya input max 6 character saja
                 style: TextStyle(color: Colors.white, fontSize: 20),
                 decoration: InputDecoration(
                   suffixIcon: GestureDetector(
@@ -85,23 +94,67 @@ class _LoginPageState extends State<LoginPage> {
                     },
                     child: Icon(obscureText ? Icons.visibility_off : Icons.visibility),
                   ),
-                  suffixIconColor: Color(0xff3085fe),
+                  suffixIconColor: Colors.white,
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(
                       width: 3,
-                      color: Color(0xff3085fe),
+                      color: Colors.white,
                     ),
-                    borderRadius: BorderRadius.circular(20),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
                       width: 3,
-                      color: Color(0xff3085fe),
+                      color: Colors.white,
                     ),
-                    borderRadius: BorderRadius.circular(20),
+                    
                   ),
                 ),
                 obscureText: obscureText,
+              ),
+              SizedBox(height: 25),
+              Center(
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.white, width: 2),
+                  ),
+                  child: Text(
+                    'L O G I N',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Center(
+                child: Text(
+                  'S I D A R',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Center(
+                child: Text(
+                  'Copyright @ 2024 Indarco.',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  'All Right Reserved',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ],
           ),
